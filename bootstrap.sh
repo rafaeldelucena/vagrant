e03d5d8cfd9287a5aed6ef4599e4087162bdaa3e
Rafael de Lucena Valle <rafaeldelucena@gmail.com>
2014-06-20 23:55:58 -0300

    Fixing bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,26 @@
 USER="rafaeldelucena"
 PASSWORD="r0ck&R0!!"
+VERSION="squeeze"
 
-sudo apt-get -y install git-core vim avrdude avrdude-doc binutils-avr avr-libc gcc-avr bin86 qemu ctags msp430mcu msp430-libc gcc-msp430 gdb-msp430 binutils-msp430 mspdebug apt-file
 
-git clone https://github.com/cpputest/cpputest.git
-cd cpputest
-./configure
-make
-sudo make install
-cd ~/.vimrc
-ctags "-R --sort=yes --c++-kinds=+p --fields=+iaS --extra=+q --language-force=C++ -f  cpputest ~/cpputest/"
-cd
-git clone https://$(USER):$(PASSWORD)@bitbucket.org/rafaeldelucena/configurations.git
-cp ~/configurations/* ~/
+sudo echo "deb http://www.emdebian.org/debian ${VERSION} main" >> /etc/apt/sources.list
+
+sudo apt-get update
+
+# dev tools
+sudo apt-get -y install git-core vim
+
+# toolchains
+sudo apt-get -y avrdude avrdude-doc binutils-avr avr-libc gcc-avr
+sudo apt-get -y install bin86 ctags
+sudo apt-get -y install msp430mcu msp430-libc gcc-msp430 gdb-msp430 binutils-msp430 mspdebug
+
+git clone https://github.com/cpputest/cpputest.git && cd cpputest && ./configure && make && sudo make install
+mkdir -f ~/.vim/tags && cd ~/.vim/tags && ctags "-R --sort=yes --c++-kinds=+p --fields=+iaS --extra=+q --language-force=C++ -f  cpputest ~/cpputest/"
+
+cd ~/ && git clone https://${USER}:${PASSWORD}@bitbucket.org/rafaeldelucena/configurations.git && cp ~/configurations/* ~/
 
 sudo apt-file update
 
 wget http://rr-project.org/releases/rr-1.3.0-Linux-$(uname -m).deb
 sudo dpkg -i rr-1.3.0-Linux-$(uname -m).deb
-
-sudo add-apt-repository ppa:terry.guo/gcc-arm-embedded
-sudo apt-get update
-sudo apt-get -y install gcc-arm-none-eabi gdb-arm-none-eabi
